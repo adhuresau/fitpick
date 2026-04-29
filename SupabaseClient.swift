1008d8b581a402fcaa7fb88eef0321ef1081e519
abartek-5678 <allybartek@gmail.com>
2026-04-29 12:51:58 -0400

    Initialize Supabase client in SupabaseClient.swift
    This connects our project to the database

---
+++ SupabaseClient.swift
@@ -0,0 +1,11 @@
+//
+//  Supabase.swift
+//  Final_LoginPage
+//
+//  Created by user940518 on 4/28/26.
+//
+
+import Supabase
+import Foundation
+
+let supabase = SupabaseClient(supabaseURL: URL(string: "https://ekrevjwiiowilnyeller.supabase.co")!, supabaseKey: "sb_publishable_Z64TYRxGpBtnfEyAf27NkQ_nJgsrBXv")
